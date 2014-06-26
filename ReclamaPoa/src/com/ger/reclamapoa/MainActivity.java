47de463ecb5a2c571eebf2816fada28832d9aa5a
markartur <mario.dornelles@gmail.com>
2014-06-26 17:35:10 -0300

    Final fix

--- ReclamaPoa/src/com/ger/reclamapoa/MainActivity.java
+++ ReclamaPoa/src/com/ger/reclamapoa/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends ActionBarActivity {
             	 for(CAR c:lista){
             		 lattemp = c.lat-MyLocationListener.latitude;
             		 lontemp = c.lon-MyLocationListener.longitude;
-            		 distanciatemp = lattemp+lontemp;
+            		 distanciatemp = Math.abs(lattemp)+Math.abs(lontemp);
             		 
             		 if(Math.abs(distanciatemp)<Math.abs(menordistancia)){
             			 menordistancia = distanciatemp;
